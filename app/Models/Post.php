8fc2f030debb9071ef3fd444932bcc4e22ad58ff
AlexandersGitHubWorks <alexanderszzzzzz@gmail.com>
2021-07-22 22:55:15 +0300

    add meta data

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,24 +2,51 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
+    public $title;
+
+    public $excerpt;
+
+    public $body;
+
+    public $slug;
+
+    public $date;
+
+    public function __construct($title, $excerpt, $body, $slug, $date)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->body = $body;
+        $this->slug = $slug;
+        $this->date = $date;
+    }
+
     public static function all()
     {
-        $files = File::files(resource_path('posts'));
+        $posts = collect(File::files(resource_path('posts')))
+            ->map(function ($file) {
+                return YamlFrontMatter::parseFile($file);
+            })
+            ->map(function ($document) {
+                return new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->body(),
+                    $document->slug,
+                    $document->date
+                );
+            });
 
-        return array_map(fn($file) => $file->getContents(), $files);
+        return cache()->remember('posts', now()->addMinute(), fn() => $posts);
     }
 
     public static function find($slug)
     {
-        if (! file_exists($file = resource_path("posts/$slug.html"))) {
-            throw new ModelNotFoundException;
-        }
-
-        return cache()->remember("post.{$slug}", now()->addMinute(), fn() => file_get_contents($file));
+        return static::all()->firstWhere('slug', $slug) ?? abort(404);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,7 +6,10 @@
 
 <body>
     <article>
-        {!! $post !!}
+
+        <h1>{{ $post->title }}</h1>
+
+        <div>{!! $post->body !!}</div>
 
         <a href="/">Go Back</a>
     </article>
