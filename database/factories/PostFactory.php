bce0c6ae580dae7b2f5ed0b136068f207948b228
AlexandersGitHubWorks <alexanderszzzzzz@gmail.com>
2021-07-27 17:23:57 +0300

    add factories

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $title = $this->faker->sentence();
+
+        $sentence = $this->faker->sentence(50);
+
+        return [
+            'title'       => $title,
+            'slug'        => Str::kebab($title),
+            'body'        => "<p>{$sentence}</p>",
+            'excerpt'     => Str::words($sentence, 10),
+            'user_id'     => User::factory(),
+            'category_id' => Category::factory(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,39 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $user = User::factory()->create();
-
-         $family = Category::create([
-             'name' => 'Family',
-             'slug' => 'family',
+         $user = User::factory()->create([
+             'name' => 'John Doe',
          ]);
 
-         $work = Category::create([
-             'name' => 'Work',
-             'slug' => 'work',
-         ]);
-
-         $hobbies = Category::create([
-             'name' => 'Hobbies',
-             'slug' => 'hobbies',
-         ]);
-
-         Post::create([
+         Post::factory(5)->create([
              'user_id' => $user->id,
-             'category_id' => $family->id,
-             'slug' => 'family-post',
-             'title' => 'My Family Post',
-             'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
-             'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
-         ]);
-
-         Post::create([
-             'user_id' => $user->id,
-             'category_id' => $work->id,
-             'slug' => 'work-post',
-             'title' => 'My Work Post',
-             'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
-             'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
          ]);
     }
 }
