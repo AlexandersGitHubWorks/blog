b75b9dbc3f81f211212044c0225182251a5818c2
AlexandersGitHubWorks <alexanderszzzzzz@gmail.com>
2021-08-07 16:27:43 +0300

    improve dropdown.blade.php

--- resources/views/components/form/dropdown.blade.php
+++ resources/views/components/form/dropdown.blade.php
@@ -3,16 +3,16 @@
 <x-form.field>
     <x-form.label name="{{ $name }}" label="{{ $label }}"/>
 
-    <select name="{{ $name }}"
-            class="border border-gray-200 p-3 w-full text-sm rounded"
-    >
-        @foreach($items as $item)
-            <option
-                value="{{ $item->{$inputValue} }}"
-                {{ old('category_id') == $item->{$inputValue} ? 'selected' : '' }}
-            >{{ $item->{$inputName} }}</option>
-        @endforeach
-    </select>
+    <div class="border border-gray-200 pr-3 w-full text-sm rounded">
+        <select name="{{ $name }}" class="w-full py-3 pl-3">
+            @foreach($items as $item)
+                <option
+                    value="{{ $item->{$inputValue} }}"
+                    {{ old('category_id') == $item->{$inputValue} ? 'selected' : '' }}
+                >{{ $item->{$inputName} }}</option>
+            @endforeach
+        </select>
+    </div>
 
     <x-form.error name="{{ $name }}"/>
 </x-form.field>
