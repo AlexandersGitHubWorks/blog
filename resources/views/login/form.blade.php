dfadde7b3af378f4766db59af1c00d89cb0f7d0b
AlexandersGitHubWorks <alexanderszzzzzz@gmail.com>
2021-09-21 17:05:35 +0300

    add user test credentials for visibility

--- resources/views/login/form.blade.php
+++ resources/views/login/form.blade.php
@@ -1,7 +1,10 @@
 <x-app>
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         <div class="max-w-3xl border border-gray-200 rounded p-12 mx-auto">
-            <h1 class="text-lg font-bold mb-8 pb-2 border-b">Log In</h1>
+            <h1 class="text-lg font-bold mb-8 pb-2 border-b">
+                Log In
+                <span class="text-sm font-normal">(Test User: admin@example.com, 123123)</span>
+            </h1>
 
             <form method="POST" action="{{ route('login.attempt') }}">
                 @csrf
